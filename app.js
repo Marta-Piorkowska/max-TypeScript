584b9bb317c4022cb85d4540c6ba64c574e286a2
Marta Piórkowska <marta@piorkowskam-l.skycash.local>
2019-09-09 09:40:53 +0200

    more advanced types

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // number
 var myAge = 25;
 // myAge: 'Marta';
+//  string
+var myName = "Marta";
+// myName = 30;
 // boolean
 var hasHobbyies = false;
 // hasHobbyies = 1;
@@ -16,3 +19,82 @@ hobbies = [100];
 var adress = ["street", 90];
 //  adress = [60, 'street'];
 // enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Orange"] = 1] = "Orange";
+    Color[Color["Green"] = 2] = "Green"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Orange;
+console.log(myColor);
+var otherColor;
+(function (otherColor) {
+    otherColor[otherColor["Red"] = 0] = "Red";
+    otherColor[otherColor["Orange"] = 100] = "Orange";
+    otherColor[otherColor["Green"] = 101] = "Green";
+    otherColor[otherColor["Blue"] = 3] = "Blue";
+    otherColor[otherColor["Grey"] = 4] = "Grey"; //4
+})(otherColor || (otherColor = {}));
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+// function
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+//  void (jeżeli finkcja nic nie zwraca!)
+function sayHello() {
+    console.log("Hello");
+    // return 'hello' (error, nie może byc return)
+}
+// argument type
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(1, "Max")); // NaN
+//
+function multiply2(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply2(1, 3));
+// function types
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply2;
+console.log(myMultiply(2, 4));
+// objects
+var userData = {
+    name: "Marta",
+    age: 25
+};
+// userData = {
+//     a: 'Marta',
+//     b: 25
+// };
+//  complex object
+var complex = {
+    data: [100, 1, 2],
+    autput: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 1, 2],
+    autput: function (all) {
+        return this.data;
+    }
+};
+// unin types
+// może byc tylko jednym z podanych typów, dla tego boolean ma error
+var myRealRealAge = 27;
+myRealRealAge = "27";
+// myRealRealAge = true;
+// check types
+var finaleValue = 30;
+if (typeof finaleValue == "number") {
+    console.log("fianle value is a number");
+}
--- app.ts
+++ app.ts
@@ -2,6 +2,10 @@
 let myAge: number = 25;
 // myAge: 'Marta';
 
+//  string
+let myName: string = "Marta";
+// myName = 30;
+
 // boolean
 let hasHobbyies: boolean = false;
 // hasHobbyies = 1;
@@ -42,3 +46,74 @@ let car: any = "BMW";
 console.log(car);
 car = { brand: "BMW", series: 3 };
 console.log(car);
+
+// function
+function returnMyName(): string {
+  return myName;
+}
+console.log(returnMyName());
+
+//  void (jeżeli finkcja nic nie zwraca!)
+function sayHello(): void {
+  console.log("Hello");
+  // return 'hello' (error, nie może byc return)
+}
+
+// argument type
+function multiply(value1, value2): number {
+  return value1 * value2;
+}
+console.log(multiply(1, "Max")); // NaN
+//
+function multiply2(value1: number, value2: number): number {
+  return value1 * value2;
+}
+console.log(multiply2(1, 3));
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply2;
+console.log(myMultiply(2, 4));
+
+// objects
+let userData: { name: string; age: number } = {
+  name: "Marta",
+  age: 25
+};
+// userData = {
+//     a: 'Marta',
+//     b: 25
+// };
+
+//  complex object
+let complex: { data: number[]; autput: (all: boolean) => number[] } = {
+  data: [100, 1, 2],
+  autput: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+// complex = {};
+
+// type alias
+type Complex = { data: number[]; autput: (all: boolean) => number[] };
+
+let complex2: Complex = {
+  data: [100, 1, 2],
+  autput: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+
+// unin types
+// może byc tylko jednym z podanych typów, dla tego boolean ma error
+let myRealRealAge: number | string = 27;
+myRealRealAge = "27";
+// myRealRealAge = true;
+
+// check types
+let finaleValue = 30;
+if (typeof finaleValue == "number") {
+  console.log("fianle value is a number");
+}
