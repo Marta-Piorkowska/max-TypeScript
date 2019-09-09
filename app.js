8f7addb3b69ce3f36117b09c2a5861158857f3f1
Marta Piórkowska <marta@piorkowskam-l.skycash.local>
2019-09-09 08:42:24 +0200

    simple types

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// number
+var myAge = 25;
+// myAge: 'Marta';
+// boolean
+var hasHobbyies = false;
+// hasHobbyies = 1;
+//assign types
+var myRealAge;
+myRealAge = 25;
+// myRealAge= '25';
+// array
+var hobbies = ["cooking", "reading"];
+hobbies = [100];
+// hobbies = 100;
+// tuples
+var adress = ["street", 90];
+//  adress = [60, 'street'];
+// enum
--- app.ts
+++ app.ts
@@ -1,1 +1,44 @@
-console.log("hello");
+// number
+let myAge: number = 25;
+// myAge: 'Marta';
+
+// boolean
+let hasHobbyies: boolean = false;
+// hasHobbyies = 1;
+
+//assign types
+let myRealAge: number;
+myRealAge = 25;
+// myRealAge= '25';
+
+// array
+let hobbies: any[] = ["cooking", "reading"];
+hobbies = [100];
+// hobbies = 100;
+
+// tuples
+let adress: [string, number] = ["street", 90];
+//  adress = [60, 'street'];
+
+// enum
+enum Color {
+  Red, // 0
+  Orange, // 1
+  Green // 2
+}
+let myColor: Color = Color.Orange;
+console.log(myColor);
+
+enum otherColor {
+  Red, // 0
+  Orange = 100, // 100
+  Green, // 101
+  Blue = 3, // 3
+  Grey //4
+}
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
